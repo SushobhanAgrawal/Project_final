8b61baacaa710f8a64353abcd0c6b56e10cad26d
Sushobhan Agrawal <sa929@njit.edu>
2017-05-05 15:42:28 -0400

    register.php

---
+++ view/register.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Registration</title>
+<link rel="stylesheet" href="css/style.css" />
+</head>
+<div class="form">
+<h1>Registration</h1>
+<form name="registration" action="" method="post">
+First Name : &nbsp<input type="text" name="fname" placeholder="Fname" required /><br><br>
+Last Name : &nbsp<input type="text" name="lname" placeholder="Lname" required /><br><br>
+User Name : <input type="text" name="username" placeholder="Username" required /><br><br>
+Email : <input type="email" name="email" placeholder="Email" required /><br><br>
+Password : <input type="password" name="password" placeholder="Password" required /><br><br>
+<input type="submit" name="submit" value="Register" /><br>
+</form>
+</div>
+</html>
