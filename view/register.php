e9f324b44939b390c0c76ad7a8908706ea42ad08
Sushobhan Agrawal <sa929@njit.edu>
2017-05-05 16:05:56 -0400

    index and register files edited

--- view/register.php
+++ view/register.php
@@ -1,4 +1,8 @@
 <!DOCTYPE html>
+<?php
+include_once("model/db_register.php");
+include_once("model/db.php");
+?>
 <html>
 <head>
 <title>Registration</title>
@@ -10,8 +14,8 @@
 First Name : &nbsp<input type="text" name="fname" placeholder="Fname" required /><br><br>
 Last Name : &nbsp<input type="text" name="lname" placeholder="Lname" required /><br><br>
 User Name : <input type="text" name="username" placeholder="Username" required /><br><br>
-Email : <input type="email" name="email" placeholder="Email" required /><br><br>
-Password : <input type="password" name="password" placeholder="Password" required /><br><br>
+Email&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp : &nbsp<input type="email" name="email" placeholder="Email" required /><br><br>
+Password&nbsp&nbsp : &nbsp<input type="password" name="password" placeholder="Password" required /><br><br>
 <input type="submit" name="submit" value="Register" /><br>
 </form>
 </div>
