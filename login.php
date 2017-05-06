c8114be4b7537c286d575f6e4a904f8dd0249eff
Sushobhan Agrawal <sa929@njit.edu>
2017-05-06 16:09:00 -0400

    edited index todo and login

--- login.php
+++ login.php
@@ -21,12 +21,61 @@ include_once("view/vlogin.php");
       </div>
     </form>
 
-    <form action="view/register.php" class="register">
+//    <form action="view/register.php" class="register">
       <div>
 <p>Not registered yet? &nbsp<a href = 'view/register.php'>Create an account</a></p>
       </div>
-    </form>
+//    </form>
 
   </div>
+</body>
+</html>
+
+/*
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>Login</title>
+<link rel="stylesheet" href="css/style.css" />
+</head>
+<body>
+<?php
+require('db.php');
+session_start();
+// If form submitted, insert values into the database.
+if (isset($_POST['username'])){
+        // removes backslashes
+	$username = stripslashes($_REQUEST['username']);
+        //escapes special characters in a string
+	$username = mysqli_real_escape_string($con,$username);
+	$password = stripslashes($_REQUEST['password']);
+	$password = mysqli_real_escape_string($con,$password);
+	//Checking is user existing in the database or not
+        $query = "SELECT * FROM `users` WHERE username='$username'
+and password='".md5($password)."'";
+	$result = mysqli_query($con,$query) or die(mysql_error());
+	$rows = mysqli_num_rows($result);
+        if($rows==1){
+	    $_SESSION['username'] = $username;
+            // Redirect user to index.php
+	    header("Location: index.php");
+         }else{
+	echo "<div class='form'>
+<h3>Username/password is incorrect.</h3>
+<br/>Click here to <a href='login.php'>Login</a></div>";
+	}
+    }else{
+?>
+<div class="form">
+<h1>Log In</h1>
+<form action="" method="post" name="login">
+<input type="text" name="username" placeholder="Username" required />
+<input type="password" name="password" placeholder="Password" required />
+<input name="submit" type="submit" value="Login" />
+</form>
+<p>Not registered yet? <a href='register.php'>Register Here</a></p>
+</div>
+<?php } ?>
 </body>
 </html>
--- todo.php
+++ todo.php
@@ -1,15 +1,19 @@
 <?php
-//include("db.php");
-//include_once("auth.php");
+include_once("db.php");
+//include("auth.php");
 ?>
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="utf-8">
-<title>Dashboard - Secured Page</title>
+<title>Todo List</title>
 <link rel="stylesheet" href="css/style.css" />
 </head>
 <body>
+<?php
+echo "Welcome";
+echo "";
+?>
 <div>
 <h2> Todo list : </h2>
 
