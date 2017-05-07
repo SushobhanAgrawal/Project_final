1c8c0b289ad129fe1d1f75988f48a40ac5a88cb0
Sushobhan Agrawal <sa929@njit.edu>
2017-05-06 20:01:49 -0400

    improved view of todo page

--- todo.php
+++ todo.php
@@ -12,20 +12,11 @@ include_once("db.php");
 
 <body>
   <?php
-  echo "<div style='text-align:right'><h1>Welcome ".$_COOKIE['login']."!</h1></div>";
+  echo "<div style='text-align:right'><h2>Welcome ".$_COOKIE['login']." !!</h2></div>";
   ?><hr>
   <h1> To-do list : </h1>
-      <div class="wrap">
- <div class="task-list">
-     <ul>
-                <?php require("includes/connect.php"); ?>
-     </ul>
- </div>
-  <form class="add-new-task" autocomplete="off">
-      <input type="text" name="new-task" placeholder="Add a new item..." />
- </form>
-      </div>
-  <p><a href="todo.php">Home</a></p>
+      
+  <p><a href="index.php">Home</a></p>
   <a href="logout.php">Logout</a>
   </div>
 </body>
