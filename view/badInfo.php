540c87faafdbaa2cb7be547f0407b3bc9b7af215
Sushobhan Agrawal <sa929@njit.edu>
2017-05-06 20:02:44 -0400

    error message page corrected

--- view/badInfo.php
+++ view/badInfo.php
@@ -1,3 +1,18 @@
-<?php
-echo "Error !!!";
-?>
+<html>
+  <head>
+      <title>
+          BadInfo 
+      </title>
+      <link rel="stylesheet" href="../css/style.css" />
+  </head>
+  
+  <body>
+    <br><br><hr>
+    <p><h2>LOGIN ERROR : </h2><h3>Incorrect username or password entered</h3><br><br>
+    Please <a href = '../login.php'>try again</a>
+    OR
+    <a href = '../view/register.php'>Register</a>
+    </p>
+  </body>
+  
+</html> 
