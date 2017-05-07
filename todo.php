b062f069709ddec4e0f461b8316053dfcd0535e3
Sushobhan Agrawal <sa929@njit.edu>
2017-05-07 16:01:53 -0400

    initialized todo list

--- todo.php
+++ todo.php
@@ -13,8 +13,24 @@
   <?php
   echo "<div style='text-align:right'><h2>Welcome ".$_COOKIE['login']." !!</h2></div>";
   ?><hr>
-  <h1> To-do list : </h1>
-      
+  <main>
+
+      <h1> To-do list : </h1>
+    <table>
+        <tr>
+            <th>Name</th>
+            <th>&nbsp;</th>
+        </tr>        
+        <!-- add category rows here -->
+    </table>
+
+    <h2>Add Task</h2>
+    <!-- add code for form here -->
+
+    <p><a href="index.php?action=list_products">List</a></p>
+
+</main>
+    
   <p><a href="todo.php">Home</a></p>
   <a href="logout.php">Logout</a>
   </div>
