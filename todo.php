a3e9e95dca777a42ba68f05b7534b2a7091eeba7
Sushobhan Agrawal <sa929@njit.edu>
2017-05-07 23:26:00 -0400

    changed the path from home page to todo page

--- todo.php
+++ todo.php
@@ -13,25 +13,8 @@
   <?php
   echo "<div style='text-align:right'><h2>Welcome ".$_COOKIE['login']." !!</h2></div>";
   ?><hr>
-  <main>
-
-      <h1> To-do list : </h1>
-    <table>
-        <tr>
-            <th>Name</th>
-            <th>&nbsp;</th>
-        </tr>        
-        <!-- add category rows here -->
-    </table>
-
-    <h2>Add Task</h2>
-    <!-- add code for form here -->
-
-    <p><a href="index.php?action=list_products">List</a></p>
-
-</main>
     
-  <p><a href="todo.php">Home</a></p>
+  <p><a href="newtask.php">Todo List</a></p>
   <a href="logout.php">Logout</a>
   </div>
 </body>
