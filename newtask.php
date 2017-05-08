d77297de5d4e995b644ac5ffd6e8dbeb981896fa
Sushobhan Agrawal <sa929@njit.edu>
2017-05-08 03:06:25 -0400

    added a link to new task

--- newtask.php
+++ newtask.php
@@ -1,11 +1,11 @@
 <?php 
-//  $item_id = $_GET['item_id'];
-//  $fname = $_GET['fname'];
-//  $lname = $_GET['lname'];
-//  require('db.php');
-//  $sql = 'select * from todo where user_id = "'.$user_id.'"';
-//  $results = runQuery($sql);
-?>
+/*  $userid = $_GET['userid'];
+  $fname = $_GET['fname'];
+  $lname = $_GET['lname'];
+  require('db.php');
+  $sql = 'select * from todos where userid = "'.$userid.'"';
+  $results = runQuery($sql);
+*/?>
 
 <html>
 <head>
@@ -74,62 +74,7 @@
     </div>
   </div>
 </div>
-
-
-<br><hr><br>
-
-<div class="container">
-    <div class="row">
-      <div class="col-md-12">
-        <h4>Completed Tasks</h4>
-          <div class="table-responsive">
-            <table id="mytable" class="table table-bordred table-striped" style="color:black;">
-                <thead>
-                  <th style="width:65%">Task</th>
-                  <th style="width:10%">Due Date</th>
-                  <th style="width:10%">Edit</th>
-                  <th style="width:10%">Delete</th>
-                  <th style="width:10%">Status</th>
-                </thead>
-                <tabel>
-                   <?php foreach($results as $res):?>
-                    <tr>                    
-                      <?php $item_status = $res['isdone'];
-                        if ($item_status == 1){
-                      ?>
-                        <td> <span style='text-decoration:line-through; font-style: italic; color: #c1c1c1'><?php echo $res['task'];?></span></td>
-                        <td> <span style='text-decoration:line-through; font-style: italic; color: #c1c1c1'><?php echo $res['duedate'];?></span></td>
-                        <td>
-                          <form action = 'index.php' method = 'post' >
-                            <input type="hidden" class="btn btn-danger" name = 'action' value="edit"/>
-                            <input type="hidden" class="btn btn-danger" name = 'item_id' value="<?php echo $res['taskid']; ?>"/>
-                            <input type="submit" class="btn btn-danger" value="edit"/>
-                          </form>
-                        </td>
-                        <td>
-                          <form action = 'index.php' method = 'post' >
-                            <input type="hidden" class="btn btn-danger" name = 'action' value="delete"/>
-                            <input type="hidden" class="btn btn-danger" name = 'item_id' value="<?php echo $res['taskid'];?>"/>
-                            <input type="submit" class="btn btn-danger" value="delete"/>
-                          </form>
-                        </td>
-                        <td>
-                          <form action = 'index.php' method = 'post' >
-                            <input type="hidden" class="btn btn-danger" name = 'action' value="incomplete"/>
-                            <input type="hidden" class="btn btn-danger" name = 'item_id' value="<?php echo $res['taskid'];?>"/>
-                            <input type="submit" class="btn btn-danger" value="Incomplete"/>
-                          </form>
-                        </td>
-                      <?php } ?>
-                    </tr>
-                  <?php endforeach;?>
-                </tabel>
-
-            </table>
-          </div>
-      </div>
-    </div>
-  </div>
-</div>
+<hr>
+<a href = "comtask.php">Completed task</a>
 </body>
 </html>
