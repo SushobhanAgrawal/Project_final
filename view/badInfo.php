cc1371dc023f0f4aee4c0a54fe6040fc339ad25d
Sushobhan Agrawal <sa929@njit.edu>
2017-05-07 21:50:39 -0400

    error message css

--- view/badInfo.php
+++ view/badInfo.php
@@ -3,15 +3,19 @@
       <title>
           BadInfo 
       </title>
+      <link rel="stylesheet" href="../css/error.css" />
       <link rel="stylesheet" href="../css/style.css" />
   </head>
   
   <body>
     <br><br><hr>
+    <div class="alert">
+      <span class="closebtn" onclick="this.parentElement.style.display='none';">&times;</span> 
     <p><h2>LOGIN ERROR : </h2><h3>Incorrect username or password entered</h3><br><br>
-    Please <a href = '../login.php'>try again</a>
+    </div>    
+    Please <a href = '../login.php'><b>try again</b></a>
     OR
-    <a href = '../view/register.php'>Register</a>
+    <a href = '../view/register.php'><b>Register</b></a>
     </p>
   </body>
   
