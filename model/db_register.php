58783bfe0b5d07a569cc78f525af89afe9975066
Sushobhan Agrawal <sa929@njit.edu>
2017-05-08 22:58:30 -0400

    corrected some fields

--- model/db_register.php
+++ model/db_register.php
@@ -1,13 +1,16 @@
 <?php
-function createUser($username, $password,$firstname,$lastname,$email){
+function createUser($username, $password,$firstname,$lastname,$email,$pnum,$bday,$gender){
  global $db;
- $query = 'insert into users (username,password,fname,lname,email) values (:username,:password,:firstname,:lastname,:email)';
+ $query = 'insert into users (username,password,fname,lname,email, pnum, bday, gender) values (:username,:password,:firstname,:lastname,:email,:pnum,:bday,:gender)';
 $statement = $db-> prepare($query);
 $statement->bindValue(':username',$username);
 $statement->bindValue(':password',$password);
 $statement->bindValue(':firstname',$firstname);
 $statement->bindValue(':lastname',$lastname);
 $statement->bindValue(':email',$email);
+$statement->bindValue(':pnum',$pnum);
+$statement->bindValue(':bday',$bday);
+$statement->bindValue(':gender',$gender);
 $statement->execute();
 $statement->closeCursor();
 return true;
